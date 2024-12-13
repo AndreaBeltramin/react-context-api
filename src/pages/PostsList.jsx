0b11c1c667a21a461bd4215c85054587f2360684
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-13 16:39:05 +0100

    eliminazione contesto da PostList perchè già presente nel componente Card

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,8 +1,6 @@
-import { postContext } from "../contexts/PostContext";
 import PostCard from "../contexts/PostCard";
 
 export default function PostsList() {
-	const { posts } = postContext();
 	return (
 		<div>
 			<h1>Lista dei Post: </h1>
