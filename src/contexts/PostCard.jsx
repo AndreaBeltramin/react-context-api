588094a3363fbb9f62ec9e7ed9d979eb17a18b08
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-13 16:36:19 +0100

    uso del componente card nella lista dei post

--- src/contexts/PostCard.jsx
+++ src/contexts/PostCard.jsx
@@ -1,22 +1,27 @@
 import { postContext } from "./PostContext";
 
 export default function PostCard() {
-	const { post } = postContext();
+	const { posts } = postContext();
 
 	return (
-		<div className="col" key={post.id}>
-			<div className="card">
-				//
-				<img src={post.img} className="card-img-top" alt="..." />
-				<div className="card-body">
-					<h5 className="card-title">{post.title.toUpperCase()}</h5>
-					<div className="card-text">
-						{post.content}
-						<div>
-							<span className="badge bg-info p-2">{post.category}</span>
+		<div>
+			<div className="row row-cols-3 g-3">
+				{posts.map((post) => (
+					<div className="col" key={post.id}>
+						<div className="card">
+							<img src={post.img} className="card-img-top" alt="..." />
+							<div className="card-body">
+								<h5 className="card-title">{post.title.toUpperCase()}</h5>
+								<div className="card-text">
+									{post.content}
+									<div>
+										<span className="badge bg-info p-2">{post.category}</span>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	);
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -3,7 +3,6 @@ import PostsList from "./PostsList";
 export default function PostsPage() {
 	return (
 		<div className="container mt-5">
-			<h1>HomePage</h1>
 			<PostsList />
 		</div>
 	);
