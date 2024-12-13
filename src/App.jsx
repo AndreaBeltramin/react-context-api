5a34e6924b4668e74bee31b53df5a3e54340bdde
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-13 15:15:45 +0100

    creazione base di partenza

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import DefaultLayout from "./layouts/DefaultLayout";
+
 // import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ListPostPage from "./pages/ListPostPage";
 
 function App() {
-	return;
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element="{<HomePage/>}" />
-			<Route path="/aboutUs" element="{<AboutUsPage/>}" />
-			<Route path="/listProducts" element="{<ListProductsPage/>}" />
-		</Routes>
-	</BrowserRouter>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route Component={DefaultLayout}>
+					<Route path="/" Component={HomePage} />
+					<Route path="/posts" Component={ListPostPage} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/ListPostPage.jsx
@@ -0,0 +1,7 @@
+export default function ListPostPage() {
+	return (
+		<div className="container mt-5">
+			<h1>Lista dei Post</h1>
+		</div>
+	);
+}
